d151a81a6da24d0005d40e4714f38ef1abd4bac1
Cui <110284510+HACKER-Cui@users.noreply.github.com>
2025-02-18 15:04:43 +0800

    Update pre.sh

--- pre.sh
+++ pre.sh
@@ -4,8 +4,9 @@ source ~/.bashrc
 echo "source enviroment"
 sudo chmod 777 /dev/ttyACM0
 cd
-#rosrun mavros mavsys rate --all 10
+
 gnome-terminal --window -e 'bash -c "roslaunch mavros apm.launch;exec bash"' \
+rosrun mavros mavsys rate --all 10
 --tab -e 'bash -c "sleep 2; roslaunch yahboomcar_visual ar_track_csi.launch"' \
 --tab -e 'bash -c "sleep 6; rosrun iq_gnc 124GPSvisionland.cpp;exec bash"' \
 
